c18d3dd14c1aabeb9ee13ece4215ea07d887f5f3
BluessyJazz <112785703+BluessyJazz@users.noreply.github.com>
2023-11-25 21:27:04 -0500

    Agregada funcionalidad de mostrar camino mas corto Camino más corto entre km y tiempo

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ grafo.mostrar_grafo()
 ciudad1, ciudad2 = input("\nIngrese la ciudad de origen: "), input("Ingrese la ciudad de destino: ")
 
 grafo.ciudades_conectadas(ciudad1, ciudad2)
+grafo.mostrar_nodo(ciudad1)
+grafo.mostrar_nodo(ciudad2)
+grafo.calcular_distancia(ciudad1, ciudad2)
+grafo.calcular_tiempo(ciudad1, ciudad2) 
+# grafo.dijkstra(ciudad1, ciudad2)

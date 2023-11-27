9b5d04bd5259a93f88463ed2c1b15eda5c39acbc
BluessyJazz <112785703+BluessyJazz@users.noreply.github.com>
2023-11-26 21:23:24 -0500

    Agrega color al menú

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def mostrar_menu():
     print("\033[1;34m1. Mostrar grafo\033[0m")
     print("\033[1;34m2. Ver ciudades conectadas\033[0m")
     print("\033[1;34m3. Mostrar información de un nodo\033[0m")
-    print("\033[1;34m4. Calcular distancia mínima entre ciudades\033[0m")
+    print("\033[1;34m4. Calcular distancia entre ciudades\033[0m")
     print("\033[1;34m5. Calcular ruta más rápida entre ciudades\033[0m")
     print("\033[1;34m6. Salir\033[0m")
 
@@ -14,19 +14,19 @@ def opciones(opcion, grafo):
         print("\nGrafo:\n")
         grafo.mostrar_grafo()
     elif opcion == 2:
-        ciudad1 = input("\nIngrese la ciudad de origen: ").capitalize()
-        ciudad2 = input("Ingrese la ciudad de destino: ").capitalize()
+        ciudad1 = input("\033[1;35m\nIngrese la ciudad de origen: \033[0m").capitalize()
+        ciudad2 = input("\033[1;35mIngrese la ciudad de destino: \033[0m").capitalize()
         grafo.ciudades_conectadas(ciudad1, ciudad2)
     elif opcion == 3:
-        ciudad = input("\nIngrese el nombre de la ciudad: ").capitalize()
+        ciudad = input("\033[1;35m\nIngrese el nombre de la ciudad: \033[0m").capitalize()
         grafo.mostrar_nodo(ciudad)
     elif opcion == 4:
-        ciudad1 = input("\nIngrese la ciudad de origen: ").capitalize()
-        ciudad2 = input("Ingrese la ciudad de destino: ").capitalize()
+        ciudad1 = input("\033[1;35m\nIngrese la ciudad de origen: \033[0m").capitalize()
+        ciudad2 = input("\033[1;35mIngrese la ciudad de destino: \033[0m").capitalize()
         grafo.calcular_distancia(ciudad1, ciudad2)
     elif opcion == 5:
-        ciudad1 = input("\nIngrese la ciudad de origen: ").capitalize()
-        ciudad2 = input("Ingrese la ciudad de destino: ").capitalize()
+        ciudad1 = input("\033[1;35m\nIngrese la ciudad de origen: \033[0m").capitalize()
+        ciudad2 = input("\033[1;35mIngrese la ciudad de destino: \033[0m").capitalize()
         grafo.calcular_tiempo(ciudad1, ciudad2)
     elif opcion == 6:
         print("Saliendo del programa...")
